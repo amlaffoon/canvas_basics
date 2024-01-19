afffa6c45fcfe0d7b10cfb69e3edc102a003fa2b
Anna Laffoon <ree11194@gmail.com>
2024-01-19 13:46:43 -0500

    randomize color and size on mouseover

--- canvas.js
+++ canvas.js
@@ -1,37 +1,42 @@
-// relative from top left x, y, width, height
-// add color
-// c.fillStyle = "#fa34a3"
-// c.fillRect(100, 100, 120, 140);
-// c.fillStyle = "rgba(255, 0, 0, 0.5"
-// c.fillRect(400, 100, 100, 100);
-// c.fillStyle = "rgba(0, 0, 255)"
-// c.fillRect(300, 300, 200, 100);
-
-// line x, y, stroke method
-
-// c.beginPath();
-// c.moveTo(50, 300);
-// c.lineTo(300, 100);
-// c.lineTo(400, 300);
-//add color
-// c.strokeStyle = "pink"; // or rgba or hex
-// c.stroke();
-
-// for (let i = 0; i < 33; i++) {
-//     let x = Math.random() * window.innerWidth;
-//     let y = Math.random() * window.innerHeight;
-// arc / circle x, y, radius, startAngle, endAngle, drawCounterClockwise
-//separate from previous line
-//     c.arc(x, y, 30, 0, Math.PI * 2, false);
-//     c.strokeStyle = "aliceBlue";
-//     c.stroke();
-// }
-
 let canvas = document.querySelector('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
 let c = canvas.getContext('2d');
+
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+let mouse = {
+    x: undefined,
+    y: undefined
+}
+
+const maxRadius = 200;
+
+const colorArray = [
+    '#edf2fb',
+    '#e2eafc',
+    '#d7e3fc',
+    '#ccdbfd',
+    '#c1d3fe',
+    '#b6ccfe',
+    '#abc4ff',
+    // '#55a630',
+    // '#2b9348',
+    // '#007f5f',
+];
+
+window.addEventListener('mousemove', function (e) {
+    mouse.x = e.x
+    mouse.y = e.y
+})
+
+window.addEventListener('resize', function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    init();
+})
+
+
 
 function Circle(x, y, dx, dy, radius) {
     this.x = x;
@@ -39,14 +44,16 @@ function Circle(x, y, dx, dy, radius) {
     this.dx = dx;
     this.dy = dy;
     this.radius = radius;
+    this.minRadius = radius;
+    this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
 
     this.draw = function () {
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        c.strokeStyle = "blue";
-        c.stroke;
+        c.fillStyle = this.color;
         c.fill();
     }
+
     this.update = function () {
         if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
             this.dx = -this.dx;
@@ -58,19 +65,34 @@ function Circle(x, y, dx, dy, radius) {
         this.x += this.dx;
         this.y += this.dy;
 
+        // interactivity
+
+        if (mouse.x - this.x < 100 && mouse.x - this.x > -100
+            && mouse.y - this.y < 50 && mouse.y > this.y > -50) {
+            if (this.radius < maxRadius) {
+                this.radius += 3;
+            }
+        } else if (this.radius > this.minRadius) {
+            this.radius -= 1;
+        }
+
         this.draw();
     }
 }
 
 let circleArray = [];
 
-for (let i = 0; i < 100; i++) {
-    let radius = 50;
-    let x = Math.random() * (innerWidth - radius * 2) + radius;
-    let y = Math.random() * (innerHeight - radius * 2) + radius;
-    let dx = (Math.random() - 0.5) * 8;
-    let dy = (Math.random() - 0.5) * 8;
-    circleArray.push(new Circle(x, y, dx, dy, radius));
+function init() {
+    circleArray = [];
+
+    for (let i = 0; i < 800; i++) {
+        let radius = Math.random() * 3 + 1;
+        let x = Math.random() * (innerWidth - radius * 2) + radius;
+        let y = Math.random() * (innerHeight - radius * 2) + radius;
+        let dx = (Math.random() - 0.5) * 5;
+        let dy = (Math.random() - 0.5) * 5;
+        circleArray.push(new Circle(x, y, dx, dy, radius));
+    };
 }
 
 
@@ -82,5 +104,7 @@ function animate() {
         circleArray[i].update();
     }
 }
+
+init();
 
 animate();
